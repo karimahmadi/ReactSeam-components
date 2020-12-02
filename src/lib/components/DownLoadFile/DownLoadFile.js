b31fbcee499b912742579f820efbe718c57bd720
Karim Ahmadi <ahmadi.k@tiddev.com>
2020-12-02 12:54:07 +0330

    add isValid function

--- src/lib/components/DownLoadFile/DownLoadFile.js
+++ src/lib/components/DownLoadFile/DownLoadFile.js
@@ -18,36 +18,39 @@ function DownLoadFile({
   fileType = 'application/pdf',
   fileName = 'file.pdf',
   url,
+  isValid = () => true,
   ...other
 }) {
   const downLoadFile = () => {
-    axios({
-      url,
-      method,
-      responseType, // 'blob'(Browser Only), 'arraybuffer', 'text', 'document', 'json'(Default), 'stream'
-      headers: {
-        common: {
-          Authorization: authorization, // `Bearer ${localStorage.getItem('Auth-Token')}`,
+    if (isValid()) {
+      axios({
+        url,
+        method,
+        responseType, // 'blob'(Browser Only), 'arraybuffer', 'text', 'document', 'json'(Default), 'stream'
+        headers: {
+          common: {
+            Authorization: authorization, // `Bearer ${localStorage.getItem('Auth-Token')}`,
+          },
         },
-      },
-    }).then(response => {
-      if (download) {
-        const href = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement('a');
-        link.href = href;
-        link.setAttribute('download', fileName);
-        document.body.appendChild(link);
-        link.click();
-      } else {
-        // Create a Blob from the PDF Stream
-        const file = new Blob([response.data], { type: fileType });
-        // Build a URL from the file
-        const fileURL = URL.createObjectURL(file);
-        // Open the URL on new Window
-        const pdfWindow = window.open();
-        pdfWindow.location.href = fileURL;
-      }
-    });
+      }).then(response => {
+        if (download) {
+          const href = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement('a');
+          link.href = href;
+          link.setAttribute('download', fileName);
+          document.body.appendChild(link);
+          link.click();
+        } else {
+          // Create a Blob from the PDF Stream
+          const file = new Blob([response.data], { type: fileType });
+          // Build a URL from the file
+          const fileURL = URL.createObjectURL(file);
+          // Open the URL on new Window
+          const pdfWindow = window.open();
+          pdfWindow.location.href = fileURL;
+        }
+      });
+    }
   };
 
   return (
@@ -66,6 +69,7 @@ DownLoadFile.propTypes = {
   fileType: PropTypes.string,
   fileName: PropTypes.string,
   url: PropTypes.string,
+  isValid: PropTypes.func,
 };
 
 export default DownLoadFile;
